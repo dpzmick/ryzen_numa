c0b9b36941fc420b2a661c743a5f9d714e4ed6f9
David Zmick <david@dpzmick.com>
2020-05-10 13:06:33 -0500

    adequate plots produced

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 a = np.zeros( (16, 16) )
-with open("bw_baseline/out") as f:
+with open("q/reader") as f:
     for line in f.readlines():
         parts = line.split(",")
         c1 = int(parts[0])
@@ -10,7 +10,30 @@ with open("bw_baseline/out") as f:
         mean = float(parts[2])
         a[(c1, c2)] = mean
 
+for i in range(16):
+    for j in range(16):
+        if i == j:
+            a[ (i,j) ] = np.max(a)
+
 plt.imshow(a, cmap='magma_r')
 plt.colorbar(orientation='horizontal', fraction=0.07, anchor=(1.0, 0.0))
-plt.savefig('bw_baseline/plot.png')
-#plt.show()
+plt.savefig('q/readerplot.png')
+plt.clf()
+
+a = np.zeros( (16, 16) )
+with open("q/writer") as f:
+    for line in f.readlines():
+        parts = line.split(",")
+        c1 = int(parts[0])
+        c2 = int(parts[1])
+        mean = float(parts[2])
+        a[(c1, c2)] = mean
+
+for i in range(16):
+    for j in range(16):
+        if i == j:
+            a[ (i,j) ] = np.max(a)
+
+plt.imshow(a, cmap='magma_r')
+plt.colorbar(orientation='horizontal', fraction=0.07, anchor=(1.0, 0.0))
+plt.savefig('q/writerplot.png')
--- q/main.c
+++ q/main.c
@@ -1,5 +1,8 @@
 #define _GNU_SOURCE
 #include <assert.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <immintrin.h>
 #include <pthread.h>
 #include <sched.h>
 #include <stdbool.h>
@@ -7,16 +10,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <x86intrin.h>
 
 typedef uint32_t m128 __attribute((vector_size (16)));
 static_assert( sizeof(m128)*8 == 128, "!" );
 
 typedef uint32_t m256 __attribute((vector_size (32)));
 static_assert( sizeof(m256)*8 == 256, "!" );
-
-typedef uint32_t m512 __attribute((vector_size (64)));
-static_assert( sizeof(m512)*8 == 512, "!" );
 
 static uint64_t tsc_freq_khz = 3892687; // AMD
 static inline uint64_t
@@ -50,7 +53,7 @@ ticks_to_ns( uint64_t ticks )
 #define N_CHUNKS    (8192ul)
 
 // how many cache lines to read/write for each synchronized chunk
-#define CHUNK_LINES (32ul)
+#define CHUNK_LINES (8ul)
 
 // type to use in chunk
 #define CHUNK_ELEMENT_T m128
@@ -60,17 +63,21 @@ ticks_to_ns( uint64_t ticks )
 
 #define TRANSFER_MB ( ( N_CHUNKS*N_ELTS*sizeof( CHUNK_ELEMENT_T ) )/1024/1024 )
 
-#define TRIALS (50)
+// this test is very sensitive to other operations on the system
+// run a very large number of trials
+#define TRIALS (500)
 
 typedef struct {
-  uint64_t ready;
-  char     pad[ CACHE_LINE- sizeof( uint64_t ) ];
-} ready_bit_t;
+  // mark if the data in the chunk is ready
+  uint64_t        ready;
 
-typedef struct {
-  _Alignas(CACHE_LINE) ready_bit_t     readies[ N_CHUNKS ];
-  _Alignas(CACHE_LINE) CHUNK_ELEMENT_T datas[ N_CHUNKS ][ N_ELTS ];
-} q_t;
+  // use up the rest of the cache line that contains ready bit
+  // we do not want the ready bit and the data bit to be in the same cache line
+  char            pad[ CACHE_LINE - sizeof( uint64_t ) ];
+
+  // actual data, will be the size of one cache line
+  CHUNK_ELEMENT_T data[ N_ELTS ];
+} chunk_t;
 
 static void __attribute__((noinline))
 bind( uint64_t id )
@@ -85,25 +92,11 @@ bind( uint64_t id )
   }
 }
 
-static void __attribute__((noinline))
-walk( char* mem, size_t sz )
-{
-  // touch every cache line to:
-  // 1) make sure pages are allocated by kernel.
-  // 2) bring mem into local cpu caches
-
-  for( size_t i = 0; i < sz; i += CACHE_LINE ) {
-    char tmp = mem[i];
-    TAINT(tmp); // without this, the compiler removes the load and store
-    mem[i] = tmp;
-  }
-}
-
 typedef struct {
   size_t       core;
   uint32_t*    wtr_ready;
   uint32_t*    rdr_ready;
-  q_t*         q;
+  chunk_t*     chunks;
   double*      out_rate;
 } thread_args_t;
 
@@ -112,12 +105,11 @@ writer_thread( void* arg )
 {
   thread_args_t* args      = arg;
   uint64_t       core      = args->core;
-  q_t*           q         = args->q;
+  chunk_t*       chunks    = args->chunks;
   uint32_t*      wtr_ready = args->wtr_ready;
   uint32_t*      rdr_ready = args->rdr_ready;
 
   bind( core );
-  walk( (char*)q->readies, sizeof( *q->readies )*N_CHUNKS );
 
   // the value that we will be sending to the other core
 #if CHUNK_ELEMENT_T == m128
@@ -128,26 +120,26 @@ writer_thread( void* arg )
 #error "unimpl chunk_t" #CHUNK_T
 #endif
 
+  CHUNK_ELEMENT_T c[ N_ELTS ];
+  for( size_t i = 0; i < N_ELTS; ++i ) {
+    c[ i ] = v1;
+  }
+
   __atomic_store_n( wtr_ready, 1, __ATOMIC_RELEASE );
   while( !__atomic_load_n( rdr_ready, __ATOMIC_ACQUIRE ) ) { }
 
   uint64_t start = rdtscp();
   for( size_t i = 0; i < N_CHUNKS; ++i ) {
-    CHUNK_ELEMENT_T* data  = q->datas[i];
-    uint64_t*        ready = &q->readies[i].ready;
+    chunk_t*         chunk = &chunks[i];
+    CHUNK_ELEMENT_T* data  = chunk->data;
+    uint64_t*        ready = &chunk->ready;
 
-    for( size_t j = 0; j < 1; ++j ) {
-      data[j] = v1;
-    }
+    memcpy( data, c, sizeof( c ) );
 
     // mark the chunk as ready
-    // __atomic_store_n( ready, true, __ATOMIC_RELEASE );
-
-    uint64_t one = 1;
-    __asm__ volatile("movnti %1,%0"
-             : "=m" (*ready)
-             : "r" (one)
-             : "memory");
+    // this doens't scale well as number of readers increases
+    // may be a good use of Non Temporal Stores? https://lwn.net/Articles/255364/
+    __atomic_store_n( ready, true, __ATOMIC_RELEASE );
   }
   uint64_t end = rdtscp();
 
@@ -163,33 +155,29 @@ reader_thread( void* arg )
 {
   thread_args_t* args      = arg;
   uint64_t       core      = args->core;
-  q_t*           q         = args->q;
+  chunk_t*       chunks    = args->chunks;
   uint32_t*      wtr_ready = args->wtr_ready;
   uint32_t*      rdr_ready = args->rdr_ready;
 
   bind( core );
 
-  // register to load into
-  CHUNK_ELEMENT_T v1;
+  CHUNK_ELEMENT_T c[ N_ELTS ];
 
   while( !__atomic_load_n( wtr_ready, __ATOMIC_ACQUIRE ) ) { }
   __atomic_store_n( rdr_ready, 1, __ATOMIC_RELEASE );
 
   uint64_t start = rdtscp();
   for( size_t i = 0; i < N_CHUNKS; ++i ) {
-    CHUNK_ELEMENT_T* data  = q->datas[i];
-    uint64_t*        ready = &q->readies[i].ready;
+    chunk_t*         chunk = &chunks[i];
+    CHUNK_ELEMENT_T* data  = chunk->data;
+    uint64_t*        ready = &chunk->ready;
 
     // wait until the line is ready
     while( !__atomic_load_n( ready, __ATOMIC_ACQUIRE ) ) { }
 
-    // read the entire data section into the sse register, but don't
-    // do anything with it.
     // have to convince compiler to leave this code in.
-
-    for( size_t j = 0; j < N_ELTS; ++j ) {
-      v1 = data[j]; SSE_USED( v1 );
-    }
+    memcpy( c, data, sizeof( c ) );
+    for( size_t j = 0; j < N_ELTS; ++j ) SSE_USED( c[j] );
   }
   uint64_t end = rdtscp();
 
@@ -201,51 +189,91 @@ reader_thread( void* arg )
 }
 
 static double
-one_to_all( size_t core1, size_t* rdr_cores, size_t n_rdr_cores, double* out_rdr_rates )
+one_to_all( size_t    core1,
+            size_t*   rdr_cores,
+            size_t    n_rdr_cores,
+            double*   out_rdr_rates,
+            chunk_t * chunks )
 {
   bind( core1 );
 
-  q_t* q = NULL;
-  posix_memalign( (void**)&q, CACHE_LINE, sizeof( q_t ) );
-  memset( q, 0, sizeof( *q ) ); // prefault
+  // clear memory and prefault unallocated pages
+  memset( chunks, 0, sizeof( chunk_t )*N_CHUNKS );
+
+  // remove the region from all layers of cache
+  for( size_t i = 0; i < sizeof( chunk_t )*N_CHUNKS; i+=CACHE_LINE ) {
+    char* ptr = (char*)chunks + i;
+    _mm_clflush( ptr );
+  }
 
   uint32_t      wtr_ready = 0;
   uint32_t      rdr_ready = 0;
   double        write_rate;
   thread_args_t args[ n_rdr_cores+1 ];
-  pthread_t     threads[ n_rdr_cores+1 ];
+  pthread_t     threads[ n_rdr_cores ];
+
+  args[0].core      = core1;
+  args[0].wtr_ready = &wtr_ready;
+  args[0].rdr_ready = &rdr_ready;
+  args[0].chunks    = chunks;
+  args[0].out_rate  = &write_rate;
 
   for( size_t i = 1; i < n_rdr_cores+1; ++i ) {
     args[i].core      = rdr_cores[i-1];
     args[i].wtr_ready = &wtr_ready;
     args[i].rdr_ready = &rdr_ready;
-    args[i].q         = q;
+    args[i].chunks    = chunks;
     args[i].out_rate  = &out_rdr_rates[i-1];
   }
 
-  args[0].core      = core1;
-  args[0].wtr_ready = &wtr_ready;
-  args[0].rdr_ready = &rdr_ready;
-  args[0].q         = q;
-  args[0].out_rate  = &write_rate;
-
   for( size_t i = 1; i < n_rdr_cores+1; ++i ) {
-    pthread_create( &threads[i], NULL, reader_thread, (void*)&args[i] );
+    pthread_create( &threads[i-1], NULL, reader_thread, (void*)&args[i] );
   }
 
-  pthread_create( &threads[0], NULL, writer_thread, (void*)&args[0] );
+  writer_thread( &args[0] );
 
-  for( size_t i = 1; i < n_rdr_cores+1; ++i ) {
+  for( size_t i = 0; i < n_rdr_cores; ++i ) {
     pthread_join( threads[i], NULL );
   }
 
-  pthread_join( threads[0], NULL );
-  free( q );
   return write_rate;
 }
 
 int main()
 {
+  chunk_t* chunks = NULL;
+  // posix_memalign( (void**)&chunks, CACHE_LINE, sizeof( chunk_t )*N_CHUNKS );
+  int fd = open( "/dev/hugepages/chunks", O_RDWR );
+  if( fd < 0 ) {
+    fprintf( stderr, "Failed to open %s\n", strerror( errno ) );
+    return 1;
+  }
+
+  size_t sz = (sizeof( chunk_t )*N_CHUNKS + 0x2000000) & ~(0x2000000-1);
+  assert( sz > sizeof( chunk_t )*N_CHUNKS );
+  int ret = ftruncate( fd, sz );
+  if( ret != 0 ) {
+    fprintf( stderr, "Failed to truncate to %zu %s\n",
+             sz,
+             strerror( errno ) );
+    return 1;
+  }
+
+  chunks = mmap( NULL,
+                 sz,
+                 PROT_READ | PROT_WRITE,
+                 MAP_PRIVATE | MAP_HUGETLB,
+                 fd,
+                 0 );
+
+  if( chunks == (chunk_t*)MAP_FAILED ) {
+    fprintf( stderr, "Failed to mmap %zu %s\n",
+             sizeof( chunk_t )*N_CHUNKS,
+             strerror( errno ) );
+    return 1;
+  }
+  close( fd );
+
   FILE* writer_out = fopen("writer", "w+");
   FILE* reader_out = fopen("reader", "w+");
 
@@ -260,12 +288,12 @@ int main()
 
       for( size_t trial = 0; trial < TRIALS; ++trial ) {
         double rdr_rate;
-        double wtr_rate = one_to_all( wtr, &rdr, 1, &rdr_rate );
+        double wtr_rate = one_to_all( wtr, &rdr, 1, &rdr_rate, chunks );
 
         avg_rdr += rdr_rate;
         avg_wtr += wtr_rate;
-        /* printf("wtr: %f\n", wtr_rate); */
-        /* printf("rdr: %f\n", rdr_rate); */
+
+        usleep(10); // space these out a bit incase system busy
       }
 
       fprintf( writer_out, "%zu,%zu,%f\n", i, j, avg_wtr/TRIALS);
@@ -280,6 +308,7 @@ int main()
   fclose( reader_out );
   fclose( writer_out );
 
+  // scaling test
   /* size_t wtr   = 0; */
   /* size_t rdr[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; */
   /* double cum_rate[ ARRAY_SIZE( rdr ) ]; */
@@ -289,7 +318,7 @@ int main()
   /*   double cum_wtr = 0.0; */
   /*   for( size_t trial = 0; trial < 100; ++trial ) { */
   /*     double rdr_rate[ ARRAY_SIZE( rdr ) ]; */
-  /*     double rate = one_to_all( wtr, rdr, i, rdr_rate ); */
+  /*     double rate = one_to_all( wtr, rdr, i, rdr_rate, chunks ); */
   /*     cum_wtr += rate; */
   /*     for( size_t x = 0; x < ARRAY_SIZE( rdr_rate ); ++x ) { */
   /*       cum_rate[ x ] += rdr_rate[ x ]; */
@@ -297,12 +326,12 @@ int main()
   /*   } */
 
   /*   printf( "wtr[%2zu] = %f\n", wtr, cum_wtr/100 ); */
-  /*   for( size_t j = 0; j < i; ++j ) { */
-  /*     // lies due to some readers starting late */
-  /*     if( cum_rate[j]/100 < cum_wtr/100 ) { */
-  /*       printf( "rdr[%2zu] = %f\n", rdr[ j ], cum_rate[ j ]/100 ); */
-  /*     } */
-  /*   } */
+  /*   /\* for( size_t j = 0; j < i; ++j ) { *\/ */
+  /*   /\*   printf( "rdr[%2zu] = %f\n", rdr[ j ], cum_rate[ j ]/100 ); *\/ */
+  /*   /\* } *\/ */
   /*   printf("\n"); */
   /* } */
+
+  // free( chunks );
+  munmap( chunks, sz );
 }
